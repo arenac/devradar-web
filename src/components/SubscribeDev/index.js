979c80ad93d0b8411ddb2f16f3b6b6bb5e46ea8d
Nilo Neregato <neregato.nilo@gmail.com>
2020-01-17 22:30:14 +0100

    pushing new dev to the server

--- src/components/SubscribeDev/index.js
+++ src/components/SubscribeDev/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+import api from '../../services/api';
+
 import { Title, Form, InputContianer, InputGroup, Button } from './styles';
 
 export default function SubscribeDev() {
@@ -27,6 +29,15 @@ export default function SubscribeDev() {
 
   async function handleAddDev(e) {
     e.preventDefault();
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+    setGithubUserName('');
+    setTechs('');
+    
   }
 
   return (
