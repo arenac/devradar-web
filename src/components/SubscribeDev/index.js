509467b273d70d79719beb0b2374be3d7de7b534
Nilo Neregato <neregato.nilo@gmail.com>
2020-01-17 22:07:58 +0100

    inputs states

--- src/components/SubscribeDev/index.js
+++ src/components/SubscribeDev/index.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import { Title, Form, InputContianer, InputGroup, Button } from './styles';
 
 export default function SubscribeDev() {
+  const [github_username, setGithubUserName] = useState('');
+  const [techs, setTechs] = useState('');
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
 
@@ -23,28 +25,54 @@ export default function SubscribeDev() {
 
   },[]);
 
+  async function handleAddDev(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
       <Title>Subscribe</Title>
-      <Form>
+      <Form onSubmit={handleAddDev}>
         <InputContianer>
           <label htmlFor="github_username">Github username</label>
-          <input name="github_username" id="github_username"/>
+          <input 
+            name="github_username" 
+            id="github_username"
+            value={github_username}
+            onChange={(e) => setGithubUserName(e.target.value)}
+          />
         </InputContianer>
         
         <InputContianer>
           <label htmlFor="techs">Technologies</label>
-          <input name="techs" id="techs"/>
+          <input 
+            name="techs" 
+            id="techs"
+            value={techs}
+            onChange={(e) => setTechs(e.target.value)}
+          />
         </InputContianer>
         
         <InputGroup>
           <InputContianer>
             <label htmlFor="latitude">Latitude</label>
-            <input type="number" name="latitude" id="latitude" value={latitude} onChange={(e) => setLatitude(e.target.value)}/>
+            <input 
+              type="number" 
+              name="latitude" 
+              id="latitude" 
+              value={latitude} 
+              onChange={(e) => setLatitude(e.target.value)}
+            />
           </InputContianer>
           <InputContianer>
             <label htmlFor="longitude">Longitude</label>
-            <input type="number" name="longitude" id="longitude" value={longitude} onChange={(e) => setLongitude(e.target.value)}/>
+            <input 
+              type="number" 
+              name="longitude" 
+              id="longitude" 
+              value={longitude} 
+              onChange={(e) => setLongitude(e.target.value)}
+            />
           </InputContianer>
         </InputGroup>
         <Button type="submit">Save</Button>
