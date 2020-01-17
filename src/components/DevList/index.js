fb5a0565b8ce6cb3ef2abd4164beebb14507cbd8
Nilo Neregato <neregato.nilo@gmail.com>
2020-01-17 22:48:19 +0100

    fetching devs from BE

--- src/components/DevList/index.js
+++ src/components/DevList/index.js
@@ -1,55 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+
+import api from '../../services/api';
 
 import { List, Dev, Header, Info } from './styles';
 
 export default function DevList() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
   return (
     <>
       <List>
-        <Dev>
-          <Header>
-            <img src="https://avatars0.githubusercontent.com/u/2737134?s=460&v=4" alt="github_avatar"/>
-            <Info>
-              <strong>Nilo Neregato</strong>
-              <span>Node.js, ReactJS, React Native</span>
-              <p>An enthusiastic of technology exploring Node.js, ReactJS and React Native stack.</p>
-              <a href="https://github.com/arenac">Github</a>
-            </Info>
-          </Header>
-        </Dev>
-        <Dev>
-          <Header>
-            <img src="https://avatars0.githubusercontent.com/u/2737134?s=460&v=4" alt="github_avatar"/>
-            <Info>
-              <strong>Nilo Neregato</strong>
-              <span>Node.js, ReactJS, React Native</span>
-              <p>An enthusiastic of technology exploring Node.js, ReactJS and React Native stack.</p>
-              <a href="https://github.com/arenac">Github</a>
-            </Info>
-          </Header>
-        </Dev>
-        <Dev>
-          <Header>
-            <img src="https://avatars0.githubusercontent.com/u/2737134?s=460&v=4" alt="github_avatar"/>
-            <Info>
-              <strong>Nilo Neregato</strong>
-              <span>Node.js, ReactJS, React Native</span>
-              <p>An enthusiastic of technology exploring Node.js, ReactJS and React Native stack.</p>
-              <a href="https://github.com/arenac">Github</a>
-            </Info>
-          </Header>
-        </Dev>
-        <Dev>
-          <Header>
-            <img src="https://avatars0.githubusercontent.com/u/2737134?s=460&v=4" alt="github_avatar"/>
-            <Info>
-              <strong>Nilo Neregato</strong>
-              <span>Node.js, ReactJS, React Native</span>
-              <p>An enthusiastic of technology exploring Node.js, ReactJS and React Native stack.</p>
-              <a href="https://github.com/arenac">Github</a>
-            </Info>
-          </Header>
-        </Dev>
+        {devs.map(dev => (
+          <Dev key={dev._id}>
+            <Header>
+              <img src={dev.avatar_url} alt={dev.name}/>
+              <Info>
+                <strong>{dev.name}</strong>
+                <span>{dev.techs.join(', ')}</span>
+                <p>{dev.bio}</p>
+                <a href={`https://github.com/${dev.github_username}`}>Github</a>
+              </Info>
+            </Header>
+          </Dev>
+        ))}
       </List>
     </>
   );
--- src/components/SubscribeDev/index.js
+++ src/components/SubscribeDev/index.js
@@ -37,7 +37,6 @@ export default function SubscribeDev() {
     });
     setGithubUserName('');
     setTechs('');
-    
   }
 
   return (
