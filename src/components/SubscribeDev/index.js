654bca98d061746d613e05079dbad502b85125fb
Nilo Neregato <neregato.nilo@gmail.com>
2020-01-17 22:07:30 +0100

    fetch location from browser

--- src/components/SubscribeDev/index.js
+++ src/components/SubscribeDev/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Title, Form, InputContianer, InputGroup, Button } from './styles';
 
 export default function SubscribeDev() {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+
+      }, 
+      {
+        timeout: 30000,
+      }
+    );
+
+  },[]);
+
   return (
     <>
       <Title>Subscribe</Title>
@@ -20,11 +40,11 @@ export default function SubscribeDev() {
         <InputGroup>
           <InputContianer>
             <label htmlFor="latitude">Latitude</label>
-            <input name="latitude" id="latitude"/>
+            <input type="number" name="latitude" id="latitude" value={latitude} onChange={(e) => setLatitude(e.target.value)}/>
           </InputContianer>
           <InputContianer>
             <label htmlFor="longitude">Longitude</label>
-            <input name="longitude" id="longitude"/>
+            <input type="number" name="longitude" id="longitude" value={longitude} onChange={(e) => setLongitude(e.target.value)}/>
           </InputContianer>
         </InputGroup>
         <Button type="submit">Save</Button>
