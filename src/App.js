63b8b664e7b9e91f2baf84b6aa7ec10435e466c5
Nilo Neregato <neregato.nilo@gmail.com>
2020-01-18 00:34:32 +0100

    wrap with persist and provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+
+import './config/ReactotronConfig';
+
+import { store, persistor } from './store';
 
 import GlobalStyle from './styles/global';
 
-import Page from './components/Page';
+import Main from './pages/Main';
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <Page />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <GlobalStyle />
+        <Main />
+      </PersistGate>
+    </Provider>
   );
 }
 
